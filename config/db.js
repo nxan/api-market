7b82d257415984523f98233554b23bd6bb05719b
nxan <nxan96@gmail.com>
2020-08-17 10:21:39 +0700

    update config

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const db = new Sequelize('market', 'root', 'nguyenxuanan', {
     dialect: 'mysql',
-    host: 'localhost',
+    host: '34.66.189.170',
     timestamps: false,
     port: 3306,
 });
