2a22117931271d471af4c56518e993d120d00e7f
nxan <nxan96@gmail.com>
2020-08-17 10:45:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,17 +13,17 @@ app.use(
     })
 );
 
-app.use((req, res, next) => {
-  const err = new Error('Not found!');
-  err.status = 404;
-  next(err);
-});
-
 app.use('/v1/customers', require('./routes/api/customers.route'))
 app.use('/v1/products', require('./routes/api/products.route'))
 app.use('/v1/prepaid', require('./routes/api/prepaid.route'))
 app.use('/v1/order', require('./routes/api/order.route'))
 app.use('/v1/orderdetail', require('./routes/api/order_detail.route'))
+
+app.use((req, res, next) => {
+  const err = new Error('Not found!');
+  err.status = 404;
+  next(err);
+});
 
 const PORT = process.env.PORT || 5000;
 
